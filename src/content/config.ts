3c26bca76de4ad2499144f542362af32529ad1e1
godemove <inovoni@proton.me>
2025-05-17 21:02:48 +0800

    fix style of stamp

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
-import { SITE } from "@config";
+import { defineCollection, z } from 'astro:content'
+import { SITE } from '@config'
+import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
-  type: "content_layer",
+  type: 'content_layer',
   loader: glob({
-    pattern: "**/*.md",
-    base: "./src/content/blog",
+    pattern: '**/*.md',
+    base: './src/content/blog',
   }),
   schema: z.object({
     title: z.string().default(SITE.title),
     description: z.string(),
     slug: z.string(),
     date: z.date(),
-    tags: z.array(z.string()).default(["Other"]),
+    tags: z.array(z.string()).default(['Other']),
   }),
-});
+})
 
-export const collections = { blog };
+export const collections = { blog }
